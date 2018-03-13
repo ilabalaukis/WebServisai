2e96161d207a68fdaa159b6982d60d1dc4c24be7
Misiukonytė Vitalija <vitalija.misiukonyte@mif.stud.vu.lt>
2018-03-13 16:22:26 +0200

    Pradėjau kurti albumus.

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
 from flask import Flask
 from redis import Redis
+from flask import jsonify
+
 import os
+
 app = Flask(__name__)
+
+albums = [
+			{'ID' : '1', 'Album' : '', 'Artist' : '', 'Genre' : '', 'Producer' : ''},
+			{'ID' : '1', 'Album' : '', 'Artist' : '', 'Genre' : '', 'Producer' : ''}
+			]
+
 redis = Redis(host='redis', port=6379)
 
 @app.route('/')
 def hello():
-    redis.incr('counter')
-    return 'Hi! You saw me %s times.' %redis.get('counter')
+	return 'List of albums'
+
+@app.route('/albums', methods=['GET'])
+def getAllInfo():
+	return jsonify({'Information':albums})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+	app.run(host="0.0.0.0", debug=True)
